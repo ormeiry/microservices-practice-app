e1bd6633960b74c7b3a6c9bc86c4c668f85dced1
ormeiry <ormeiry2411@gmail.com>
2020-07-10 17:19:46 +0300

    Creating more microservices, implemnting event-bus,handling traffic menualy in order to learn better how everything works, wrote all the logic for the services and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
---
+++ moderation/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const BodyParser = require('body-parser');
+
+const app = express();
+app.use(BodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'commentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log('Listening on port 4003');
+});
